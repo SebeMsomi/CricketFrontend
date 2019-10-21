2815c381ba4de6dda497b1a3b9d205789ad58298
Sebe Msomi <47317872+SebeMsomi@users.noreply.github.com>
2019-10-21 07:23:41 +0200

    Add files via upload

---
+++ CricketFrontend/src/app/book/book-service/book-data.ts
@@ -0,0 +1,48 @@
+import { Book } from './book';
+
+export const BOOK_LIST: Book[] = [
+          {
+              "id":1,
+          "isbn": "Dale ",
+          "title": "Styne",
+          "subtitle": "50",
+          "author": "6",
+        
+          "pages": 2,
+          "publisher": "1",
+          
+        },
+        {
+            "id":2,
+          "isbn": "Shaun ",
+          "title": "Pollock",
+          "subtitle": "84",
+          "author": "4",
+        
+          "pages": 2,
+          "publisher": "1",
+        },
+        {
+            "id":3,
+          "isbn": "Tsotsobe ",
+          "title": "Lonwabo ",
+          "subtitle": "14",
+          "author": "3",
+        
+          "pages": 4,
+          "publisher": "2",        
+        },
+        {
+            "id":4,
+          "isbn": "Makhaya ",
+          "title": "Ntini",
+          "subtitle": "55",
+          "author": "7",
+        
+          "pages": 5,
+          "publisher": "3",        
+        },
+        
+        
+      
+]
---
+++ CricketFrontend/src/app/book/book-service/book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    id:number,    
+    isbn: string,
+    title: string,
+    subtitle:string,
+    author: string,   
+    publisher: string,
+    pages: number,
+}
